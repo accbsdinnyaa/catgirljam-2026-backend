7960253222eebc8b2f6564e1f2ac0253c547d835
accbsdinnyaa <accountbasedinindiagtcolon3@proton.me>
2026-05-03 15:12:19 +0530

    more stuff

--- src/durable-object.ts
+++ src/durable-object.ts
@@ -1,6 +1,18 @@
 import { DurableObject } from "cloudflare:workers";
 import { INITIAL_STATE, modify, type Action, type S } from "./game";
 
+function mapNull<T extends NonNullable<unknown>, U extends NonNullable<unknown>>(
+	a: T | null, f: (t: T) => U
+): U | null {
+	if (a === null)
+		return null;
+	return f(a);
+}
+
+function makeError(error: unknown): string {
+	return JSON.stringify({ error });
+}
+
 export const INSTANCE_NAME = "catgirl";
 
 type Uuid = ReturnType<(typeof crypto)['randomUUID']>;
@@ -15,6 +27,7 @@ type Loner = {
 	info: {
 		name: string,
 		desc: string,
+		nsfw?: boolean,
 	} | null,
 };
 
@@ -27,42 +40,24 @@ function validateInfo(d: unknown): d is Exclude<Loner['info'], null> {
 type NonLoner = {
 	userId: Uuid,
 	dateId: Uuid,
+	info: Exclude<Loner['info'], null>,
 };
-
-// const lonerFromRaw = (raw: Record<string, any>): Loner => ({ ...raw, sender: null } as Loner);
 
 type Date = {
 	s: S,
 	left: Uuid;
 	right: Uuid;
+	nsfw: boolean;
 };
-
-// const dateFromRaw = (raw: Record<string, any>): Date => (raw as Date);
-
-// const lonersFromRaw = (raw: Array<any>): Loners => new Set(raw.map(lonerFromRaw));
-
-// type Dates = Array<Date>;
-// const datesFromRaw = (raw: Array<any>): Dates => raw.map(dateFromRaw);
-
-// type Pairs = Map<string, Map<string, /* index in `dates` */ number>>;
-/* const pairsFromRaw = (raw: Record<string, any>): Pairs => {
-	return new Map(
-		Object.entries(raw)
-			.map(([key, value]) =>
-				[key, new Map(Object.entries(value))])
-	);
-} */
-
-/* type Event = {
-	lonerId: number;
-	runAt: number;
-	repeatMs: number | null;
-}; */
 
 type DateId = Uuid;
 
-function isNonLoner(arg: any): arg is NonLoner {
+function isNonLoner(arg: object): arg is NonLoner {
 	return 'userId' in arg;
+}
+
+function isLoner(arg: object): arg is Loner {
+	return 'lonerId' in arg;
 }
 
 /**
@@ -79,6 +74,46 @@ export class MyDurableObject extends DurableObject {
 	// sessions: Map<WebSocket, { [key: string]: string }>;
 	private loners: Map<WebSocket, Loner>;
 	private nonLoners: Map<WebSocket, NonLoner>;
+	private pollers: Map<WebSocket, /* nsfw */ boolean>;
+
+	constructor(ctx: DurableObjectState, env: Env) {
+		super(ctx, env);
+
+		this.loners = new Map();
+		this.nonLoners = new Map();
+		this.pollers = new Map();
+
+		this.ctx.getWebSockets().forEach((ws) => {
+			const attachment: Loner | NonLoner | boolean = ws.deserializeAttachment();
+			if (typeof attachment === 'boolean') {
+				this.pollers.set(ws, attachment as boolean);
+			}
+			else if (isNonLoner(attachment)) {
+				this.nonLoners.set(ws, { ...attachment });
+			} else if (isLoner(attachment)) {
+				this.loners.set(ws, { ...attachment });
+			}
+		});
+
+		this.pollers.forEach((nsfw, ws) => {
+			ws.send(JSON.stringify(this.get_loners(nsfw)));
+		});
+
+		this.ctx.setWebSocketAutoResponse(new WebSocketRequestResponsePair('ping', 'pong'));
+	}
+
+	get_loners(nsfw: boolean): (Loner & { info: Exclude<Loner['info'], null> })[] {
+		const filter = (loner: Loner) => (
+			loner.info !== null &&
+			(nsfw || Boolean(loner.info.nsfw) === false)
+		);
+
+		return this.loners
+			.entries()
+			.map(([_, loner]: [WebSocket, Loner]) => loner)
+			.filter(filter)
+			.toArray();
+	}
 
 	// get
 	async date(id: DateId): Promise<Date | undefined>;
@@ -101,103 +136,25 @@ export class MyDurableObject extends DurableObject {
 		}
 	}
 
-	constructor(ctx: DurableObjectState, env: Env) {
-		super(ctx, env);
-
-		this.loners = new Map();
-		this.nonLoners = new Map();
-
-		this.ctx.getWebSockets().forEach((ws) => {
-			const attachment: Loner | NonLoner = ws.deserializeAttachment();
-			if (isNonLoner(attachment)) {
-				this.nonLoners.set(ws, { ...attachment });
-			} else {
-				this.loners.set(ws, { ...attachment });
-			}
-		});
-
-		this.ctx.setWebSocketAutoResponse(new WebSocketRequestResponsePair('ping', 'pong'));
-
-		ctx.blockConcurrencyWhile(async () => {
-
-			/*
-			const storedLoners = await ctx.storage.get<Array<any>>("loners");
-			// const storedDates = await ctx.storage.get<Array<any>>("dates");
-			const storedPairs = await ctx.storage.get<Record<string, any>>("pairs");
-			const storedCounter = await ctx.storage.get<number>("counter");
-
-			if (storedLoners) {
-				// TODO handle restart of DO with loners still waiting
-				this.loners = lonersFromRaw(storedLoners);
-			}
-			// if (storedDates) {
-			// 	// TODO handle restart of DO with dates still active
-			// 	this.dates = datesFromRaw(storedDates);
-			// }
-			if (storedPairs) {
-				// TODO handle restart of worker with dates still active
-				this.pairs = pairsFromRaw(storedPairs);
-			}
-			if (storedCounter) {
-				this.counter = storedCounter;
-			}
-			*/
-
-		});
-	}
-
-	/* async addLoner(loner: Loner) {
-		this.loners.add(loner);
-		await this.ctx.storage.put("loners", this.loners);
-	} */
-
-	// Schedule a one-time or recurring event
-	/* async scheduleEvent(id: number, runAt: number, repeatMs: number | null = null) {
-		await this.ctx.storage.put(`event:${id}`, { id, runAt, repeatMs });
-		const currentAlarm = await this.ctx.storage.getAlarm();
-		if (!currentAlarm || runAt < currentAlarm) {
-			await this.ctx.storage.setAlarm(runAt);
-		}
-	}
-
-
-	async alarm() {
-		const now = Date.now();
-		const events = await this.ctx.storage.list<Event>({ prefix: "event:" });
-		let nextAlarm = null;
-
-		for (const [key, event] of events) {
-			if (event.runAt <= now) {
-				await this.processEvent(event);
-				if (event.repeatMs) {
-					event.runAt = now + event.repeatMs;
-					await this.ctx.storage.put(key, event);
-				} else {
-					await this.ctx.storage.delete(key);
-				}
-			}
-			// Track the next event time
-			if (event.runAt > now && (!nextAlarm || event.runAt < nextAlarm)) {
-				nextAlarm = event.runAt;
-			}
-		}
-
-		if (nextAlarm) await this.ctx.storage.setAlarm(nextAlarm);
-	}
-	*/
-
 	async fetch(request: Request): Promise<Response> {
 		const url = new URL(request.url);
 
 		// API: Return loners
-		if (url.pathname.endsWith("/loners") && request.method === "GET") {
-			const loners = this.loners
-				.entries()
-				.map(([_, loner]) => loner)
-				.filter(loner => loner.info !== null)
-				.toArray();
+		if (url.pathname.endsWith("/watch") && request.method === "GET") {
+			if (request.headers.get("Upgrade") !== "websocket")
+				return new Response('need upgrade header for websocket', { status: 400 });
 
-			return Response.json(loners);
+			const nsfw = url.searchParams.get('nsfw')?.toLowerCase() === 'true';
+			const pair = new WebSocketPair();
+			const [client, server] = Object.values(pair);
+
+			this.ctx.acceptWebSocket(server);
+			server.serializeAttachment(nsfw);
+			this.pollers.set(server, nsfw);
+
+			server.send(JSON.stringify(this.get_loners(nsfw)));
+
+			return new Response(null, { status: 101, webSocket: client });
 		}
 
 		// API: Create a new room & wait for a date
@@ -217,8 +174,7 @@ export class MyDurableObject extends DurableObject {
 
 				// add to list of loners
 				this.loners.set(server, attachment);
-
-				server.send("pls initialize");
+				//server.send("pls initialize");
 
 				return new Response(null, { status: 101, webSocket: client });
 			}
@@ -236,8 +192,14 @@ export class MyDurableObject extends DurableObject {
 				const firstId = url.searchParams.get('lonerId');
 				if (firstId === null)
 					return new Response('?lonerId= needed', { status: 400 });
+				const nsfw = mapNull(url.searchParams.get('nsfw'), s => s.toLowerCase() === 'true');
 
-				const first = this.loners.entries().find(([_, loner]) => loner.lonerId === firstId);
+				const first: [WebSocket, Loner] | undefined = this.loners.entries()
+					.find(([_, loner]: [WebSocket, Loner]) => (
+						loner.lonerId === firstId &&
+						loner.info !== null &&
+						(nsfw || !loner.info.nsfw)
+					));
 				if (first === undefined || first[1].info === null) { // unknown or uninitialized loner
 					console.error("/date called with unknown or uninit loner", first);
 					return new Response('unknown loner', { status: 400 });
@@ -249,20 +211,39 @@ export class MyDurableObject extends DurableObject {
 				const secondId = crypto.randomUUID();
 
 				const dateId = crypto.randomUUID();
-				await this.date(dateId, { left: firstId, right: secondId, s: INITIAL_STATE });
+				const initDate: Date = {
+					left: firstId,
+					right: secondId,
+					s: INITIAL_STATE,
+					nsfw: Boolean(nsfw),
+				};
+				await this.date(dateId, initDate); // create new date
 
 				this.ctx.acceptWebSocket(server);
 
-				const firstNew: NonLoner = { userId: firstId, dateId };
+				const firstNew: NonLoner = {
+					userId: firstId, dateId, info: {
+						name: "",
+						desc: "",
+						nsfw: nsfw || undefined
+					}
+				};
 				this.loners.delete(first[0]);
+
+				this.pollers.forEach((nsfw, ws) => {
+					ws.send(JSON.stringify(this.get_loners(nsfw)));
+				});
+
 				this.nonLoners.set(first[0], firstNew);
 				first[0].serializeAttachment(firstNew);
 
-				const second: NonLoner = { userId: secondId, dateId };
+				const second: NonLoner = { userId: secondId, dateId, info: first[1].info };
 				this.nonLoners.set(server, second);
 				server.serializeAttachment(second);
 
-				first[0].send("you got date");
+				// letting the person who created the room know that the date has started by sending the
+				// initial state
+				first[0].send(JSON.stringify(initDate.s));
 
 				// add to list of loners
 				return new Response(null, { status: 101, webSocket: client });
@@ -275,8 +256,8 @@ export class MyDurableObject extends DurableObject {
 		return new Response("unknown route", { status: 400 });
 	}
 
-	getSession(ws: WebSocket): Loner | NonLoner {
-		return this.loners.get(ws) || this.nonLoners.get(ws)!;
+	getSession(ws: WebSocket): Loner | NonLoner | boolean | undefined {
+		return this.loners.get(ws) || this.nonLoners.get(ws) || this.pollers.get(ws);
 	}
 
 	/**
@@ -285,26 +266,44 @@ export class MyDurableObject extends DurableObject {
 	 */
 	async webSocketMessage(ws: WebSocket, message: string | ArrayBuffer): Promise<void> {
 		if (typeof message === 'object') {
+			console.log("can't read bytes msgs");
 			ws.send("can't read bytes msgs");
 			return;
 		}
-		const session = this.getSession(ws);
 
-		if (!isNonLoner(session)) {
+		const session = this.getSession(ws);
+		if (session === undefined) {
+			return;
+		}
+
+		// client that is just polling for loners
+		if (typeof session === 'boolean') {
+			// no reason to talk to us
+			console.log("we are not listening");
+			ws.send("i am not listening");
+			return;
+		}
+
+		if (isLoner(session)) {
 			// initialize loner
 			if (session.info === null) {
 				const messageJson = JSON.parse(message);
 				if (!validateInfo(messageJson)) {
+					console.log("bad initialization");
 					ws.send("bad initialization");
 				} else {
 					session.info = messageJson;
 					ws.serializeAttachment(session);
-					ws.send("thanks for initializing");
+					this.pollers.forEach((nsfw, ws) => {
+						ws.send(JSON.stringify(this.get_loners(nsfw)));
+					});
+					ws.send("true");
 				}
 				return;
 			}
 
 			// initialized loner has no reason to talk to us
+			console.log("dunno, wait for date");
 			ws.send("dunno, wait for date");
 			return;
 		}
@@ -318,7 +317,13 @@ export class MyDurableObject extends DurableObject {
 		const [otherWs, _] = this.nonLoners.entries().find(([_, nonLoner]) => nonLoner.userId === otherId)!;
 
 		try {
-			const action: Action | { admin: AdminMessages } | { text: string } = JSON.parse(message);
+			const action:
+				| Action
+				| { admin: AdminMessages }
+				| { text: string }
+				| { setMe: Exclude<Loner['info'], null> }
+				| { getMe: true }
+				= JSON.parse(message);
 
 			if (typeof action === 'object' && 'text' in action) {
 				otherWs.send(JSON.stringify({ text: action.text }));
@@ -332,17 +337,35 @@ export class MyDurableObject extends DurableObject {
 					// end date
 					await this.date(session.dateId, null);
 					this.nonLoners.delete(otherWs);
+					console.log("closed second person in date");
 					otherWs.close();
 					this.nonLoners.delete(ws);
+					console.log("closed first person in date");
 					ws.close();
 				}
+				return;
+			}
+			else if (typeof action === 'object' && 'setMe' in action) {
+				if (!validateInfo(action.setMe)) {
+					console.log("setMm with bad info");
+					ws.send(makeError("bad info"));
+					return;
+				}
+				session.info = { ...action.setMe, nsfw: session.info.nsfw };
+				ws.serializeAttachment(session);
+				return;
+			}
+			else if (typeof action === 'object' && 'getMe' in action) {
+				ws.send(JSON.stringify(session.info));
 				return;
 			}
 
 			const error = modify(date.s, action, isLeft);
 			if (error) {
+				console.log("error modifying state: ", error);
+				console.log("msg was:", action);
 				ws.send(`error: ${error}`);
-				return
+				return;
 			}
 			this.date(session.dateId, { ...date, s: date.s });
 
@@ -350,7 +373,8 @@ export class MyDurableObject extends DurableObject {
 			otherWs.send(JSON.stringify(date.s));
 		}
 		catch (e) {
-			ws.send(JSON.stringify({ error: `${e}` }));
+			console.log("caught error", e);
+			ws.send(JSON.stringify({ error: "`${e}`" }));
 		}
 	}
 
@@ -365,11 +389,15 @@ export class MyDurableObject extends DurableObject {
 		_wasClean: boolean,
 	): Promise<void> {
 		const session = this.getSession(ws);
+		if (typeof session === 'undefined') {
+			return;
+		}
 
-		if (code === 1005)
-			ws.close();
-		else
-			ws.close(code, reason);
+		if (typeof session === 'boolean') {
+			console.log("closed person /watch'ing");
+			// ws.close();
+			return;
+		}
 
 		if (isNonLoner(session)) {
 			console.log("removed from non-loners");
@@ -387,14 +415,29 @@ export class MyDurableObject extends DurableObject {
 					.find(([_, nonLoner]) => nonLoner.userId === otherId)!;
 				await this.date(session.dateId, null);
 				this.nonLoners.delete(otherWs);
+				console.log("closed second person in date");
 				otherWs.close();
 				this.nonLoners.delete(ws);
-				ws.close();
 			}
 		} else {
 			console.log("removed from loners");
 			this.loners.delete(ws);
+
+			this.pollers.forEach((nsfw, ws) => {
+				ws.send(JSON.stringify(this.get_loners(nsfw)));
+			});
 		}
+
+		if (isNonLoner(session))
+			console.log("closed first person in date");
+		else
+			console.log("closed loner");
+		// if (code === 1005) {
+		// 	ws.close();
+		// }
+		// else {
+		// 	ws.close(code, reason);
+		// }
 	}
 
 	/**
